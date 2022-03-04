a9fd0dc7704e406552c5c528d40e02db89a33a61
gsim1903 <gavin@internetbusker.co>
2022-03-04 10:30:15 +0100

    user check box added

--- cypress/integration/userCanSeeALayout.feature.js
+++ cypress/integration/userCanSeeALayout.feature.js
@@ -4,8 +4,14 @@ describe("Display list of products", () => {
     });
   
     it("is expected to see a header", () => {
-      cy.get("[data-cy=header]").should("contain","RockPaperScissors")
+      cy.get("[data-cy=header]").should("contain","Rock Paper Scissors")
       ;
     });
+
+    it("is expected to prompt user for input", () => {
+        cy.get("[data-cy=user_prompt]").should("contain","Enter your guess here")
+        ;
+      });
+  
 })
 
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div data-cy="header">RockPaperScissors</div> 
+    <div data-cy="header"> <h1> Rock Paper Scissors </h1> 
+
+    <h3> Please enter your guess; Rock, Paper or Scissors</h3>
+
+    <input data-cy="user_prompt" type="checkbox" value="Rock" ></input>
+    
+
+    <h3> The computer guess is: </h3>
+
+
+    </div> 
+    
+
   )
 }
 export default App
